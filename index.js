4f35c2f90c7fe5571419e21041b4be349461eed6
gich2009 <waneneamos@gmail.com>
2023-04-02 19:24:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 function isPalindrome(word) {
 
-  let bufferString = "";
-
-  for (let index = word.length - 1 ; index >= 0 ; --index){
-    bufferString += word[index];
+  for (let leftIndex = 0, rightIndex = word.length - 1 ; leftIndex < rightIndex ; ++leftIndex, --rightIndex){
+    if (word[leftIndex] !== word[rightIndex]){
+      return false;
+    }
   }
 
-  return (bufferString === word) ? true : false;
+  return true;
 }
 
 /* 
   Add your pseudocode here
   My pseudocode format resembles a recipe and is written as an ordered list: 
 
-  1. Create an empty buffer string to store the word in reverse.
-  2. Loop through the word from end to beginning accumulating each character into the buffer string.
-  3. Compare the buffer string(which is reversed) with the original. If equal return true, else return false.
+  1. Compare each letter at the beginning of the word to its corresponding letter at the end of the word: 
+     e.g 0 to (n - 1), 1 to (n - 2), 2 to (n - 3) and so on.
+  2. If they are all equal, then the word is a palindrome, return true. Else return false.
 */
 
 /*
   Add written explanation of your solution here
-  The function takes the word argument and loops through in reverse storing the result in a buffer string.
-  Once the buffer string is fully populated, the function compares the word argument withe the buffer string
-  and returns true if they are equal, otherwise, it returns false.
+
+  1. Create two indices, one starting from the beginning of the word (called leftIndex) and the other from the end (called rightIndex).
+  2. Compare the letter at the leftIndex and the rightIndex positions of the word. If they are not equal, the word is not a palindrome, return false.
+  3. If the loop terminates, then it is a palindrome, return true because all the correponding letters were found to be equal. 
+     The stop clause for the loop is when the leftIndex meets the rightIndex in the middle or passes the rightIndex in the middle which is expressed as
+     leftIndex < rightIndex.
+     
+  The time complexity for my function would be O(n/2) which is O(n), linear time execution when the constants are ignored. However, it would execute significantly 
+  faster than a typical O(n) time complexity algorithm because the time of execution grows with only half the input size and not the full input size. 
+  For very large strings, this would be a huge perfomance difference. 
+
+  The memory complexity for my function is O(2) since it only requires space for 2 variables, leftIndex and rightIndex, the comparison is done in place
+  without a need to copy the word argument. This can be expressed as O(2 * 1) and ignoring the constant 2 gives O(1). Constant memory execution.
+
 
   The function can also be made case insensitive by preprocessing the word argument to become a single case 
   (either upper or lower) before executing the algorithm. An illustration is given below with the key statement being line 2: 
 
   function isPalindrome(word) {
-    
-1    word = word.toUpperCase();      //or word = word.toLowerCase();
-    
-2    let bufferString = "";
 
-3    for (let index = word.length - 1 ; index >= 0 ; --index){
-4      bufferString += word[index];
-5    }
+  word = word.toLowerCase(); //or word = word.toUpperCase();
+    
+  for (let leftIndex = 0, rightIndex = word.length - 1 ; leftIndex < rightIndex ; ++leftIndex, --rightIndex){
+    if (word[leftIndex] !== word[rightIndex]){
+      return false;
+    }
+  }
 
-6    return (bufferString === word) ? true : false;
+  return true;
   }
 */
 
@@ -58,6 +69,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("nine"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("sir"));
 }
 
 module.exports = isPalindrome;
